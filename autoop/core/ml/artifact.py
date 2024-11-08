34fbf0cc70dc353498d8930eeb77c21dc2b41ae9
viktorkotuliak <viktor.kotuliak@gmail.com>
2024-11-08 18:17:53 +0100

    style fix

--- autoop/core/ml/artifact.py
+++ autoop/core/ml/artifact.py
@@ -1,9 +1,11 @@
 from typing import Dict, List
-from pydantic import BaseModel, Field, field_validator
+from pydantic import BaseModel, Field
 import base64
+
 
 class Artifact(BaseModel):
     """Description: Base class for all artifacts."""
+
     asset_path: str = Field()
     data: bytes = Field()
     version: str = Field(default="1.0.0")
@@ -11,20 +13,17 @@ class Artifact(BaseModel):
     type: str = Field(default="")
     tags: List[str] = Field(default=list)
 
-    # @field_validator('type')
-    # def validate_type(cls, v):
-    #     if ':' not in v:
-    #         raise ValueError("Type must follow the format '<category>:<framework>'.")
-    #     return v
-    
     @property
     def id(self) -> str:
+        """Returns the unique identifier for the artifact"""
         path = base64.b64encode(self.asset_path.encode()).decode()
         return f"{path}:{self.version}"
-    
+
     def read(self) -> bytes:
+        """Reads the artifact"""
         return self.data
-    
+
     def save(self, data: bytes) -> bytes:
+        """Saves the artifact"""
         self.data = data
         return self.data
--- autoop/core/ml/dataset.py
+++ autoop/core/ml/dataset.py
@@ -1,28 +1,44 @@
 from autoop.core.ml.artifact import Artifact
-from abc import ABC, abstractmethod
 import pandas as pd
 import io
 
-class Dataset(Artifact):
 
-    def __init__(self, *args, **kwargs):
+class Dataset(Artifact):
+    """Dataset"""
+
+    def __init__(self, *args, **kwargs) -> None:
         super().__init__(type="dataset", *args, **kwargs)
 
     @staticmethod
-    def from_dataframe(data: pd.DataFrame, name: str, asset_path: str, version: str="1.0.0"):
+    def from_dataframe(data: pd.DataFrame, name: str, asset_path: str, 
+                       version: str = "1.0.0") -> "Dataset":
+        """
+        Creates a dataset from a pandas DataFrame
+
+        Args:
+            data (pd.DataFrame): DataFrame
+            name (str): Name of the dataset
+            asset_path (str): Path to the dataset asset
+            version (str, optional): Version of the dataset. Defaults to "1.0.0"
+        
+        Returns:
+            Dataset: Dataset artifact
+        """
         return Dataset(
             name=name,
             asset_path=asset_path,
             data=data.to_csv(index=False).encode(),
             version=version,
         )
-        
+ 
     def read(self) -> pd.DataFrame:
+        """Reads the dataset from the artifact"""
         bytes = super().read()
         csv = bytes.decode()
         return pd.read_csv(io.StringIO(csv))
-    
+
     def save(self, data: pd.DataFrame) -> bytes:
+        """Saves the dataset to the artifact"""
         bytes = data.to_csv(index=False).encode()
         return super().save(bytes)
-    
+  
