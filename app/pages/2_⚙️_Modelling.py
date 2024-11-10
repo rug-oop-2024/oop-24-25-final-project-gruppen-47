432b9c757f687305dd4c1d914eae05b36fdf38e5
viktorkotuliak <viktor.kotuliak@gmail.com>
2024-11-10 16:08:26 +0100

    Renamed files modelling and Deployment

---
+++ app/pages/2_⚙️_Modelling.py
@@ -0,0 +1,125 @@
+import streamlit as st
+
+from app.core.system import AutoMLSystem
+from autoop.core.ml.metric import (
+    Accuracy,
+    MeanAbsoluteError,
+    MeanSquaredError,
+    Precision,
+    Recall,
+    RootMeanSquaredError,
+)
+from autoop.core.ml.model.classification.k_nearest_neighbors import (
+    KNearestNeighbors,
+)
+from autoop.core.ml.model.classification.random_forest_wrap import RandomForest
+from autoop.core.ml.model.regression.multiple_linear_regression import (
+    MultipleLinearRegression,
+)
+from autoop.core.ml.model.regression.ridge_wrap import Ridge
+from autoop.core.ml.model.regression.lasso_wrap import Lasso
+from autoop.core.ml.model.classification.naive_bayes_wrap import (
+    NaiveBayesModel,
+)
+from app.core.modelling.datasets import pick_dataset, select_features
+from autoop.core.ml.pipeline import Pipeline
+
+automl = AutoMLSystem.get_instance()
+
+st.set_page_config(page_title="Modelling", page_icon="📈")
+
+
+def write_helper_text(text: str):
+    st.write(f'<p style="color: #888;">{text}</p>', unsafe_allow_html=True)
+
+
+st.write("# ⚙ Modelling")
+write_helper_text(
+    "In this section, you can design a machine learning pipeline to train a model on a dataset."
+)
+
+selected_dataset = pick_dataset()
+
+input_features, target_feature = select_features(selected_dataset)
+
+
+if target_feature.type == "categorical":
+    st.write(
+        "Based on the selected input features, you have to use classification models."
+    )
+    model = st.selectbox(
+        "Please select a model.",
+        [KNearestNeighbors, NaiveBayesModel, RandomForest],
+        format_func=lambda model: model.__name__,
+    )
+    metrics = st.multiselect(
+        "Please select the metrics to evaluate the model.",
+        [Accuracy, Precision, Recall],
+        format_func=lambda metric: metric.__name__,
+    )
+else:
+    model = st.selectbox(
+        "Please select a model.",
+        [Lasso, Ridge, MultipleLinearRegression],
+        format_func=lambda model: model.__name__,
+    )
+    metrics = st.multiselect(
+        "Please select the metrics to evaluate the model.",
+        [MeanSquaredError, MeanAbsoluteError, RootMeanSquaredError],
+        format_func=lambda metric: metric.__name__,
+    )
+
+# TODO change metrics to have attribute categorical or numerical
+
+split = st.slider(
+    "Please select the percentage of the dataset that will go for training.",
+    0,
+    100,
+    50,
+)
+
+st.write(f"## Pipeline Configuration")
+
+st.write(f"**Dataset**: {selected_dataset.name}")
+st.write(f"**Model**: {model.__name__}")
+st.write(f"**Input Features**: {', '.join([x.name for x in input_features])}")
+st.write(f"**Target Feature**: {target_feature.name}")
+st.write(f"**Split**: {split}%")
+st.write(f"**Metrics**: {', '.join([x.__name__ for x in metrics])}")
+
+st.write(f"## Results")
+
+if 'pipeline' not in st.session_state:
+    st.session_state.pipeline = None
+
+try:
+    if st.button("Run Pipeline"):
+        st.session_state.pipeline = Pipeline(
+            dataset=selected_dataset,
+            model=model(),
+            input_features=input_features,
+            target_feature=target_feature,
+            metrics=list(metric() for metric in metrics),
+            split=split / 100,
+        )
+        results = st.session_state.pipeline.execute()
+        st.write(f"**Model Parameters**: {st.session_state.pipeline.model.parameters}")
+        st.write(f"**Metrics on evaluation set**: {results["metrics_on_evaluation_set"]}")
+        st.write(f"**Metrics on training set**: {results["metrics_on_training_set"]}")
+        st.write(f"**Predictions**: {results["predictions"]}")
+except ValueError:
+    st.write("Error: please fill all the required fields.")
+
+try:
+    artifact_name = st.text_input("Enter your pipleine name:")
+    artifact_version = st.text_input("Enter your pipleine version:")
+
+    if st.button("Save Pipeline"):
+        assert artifact_name != "", st.write("Field Name cannot be empty.")
+        assert artifact_version != "", st.write("Field Version cannot be empty.")
+        new_artifact = st.session_state.pipeline.to_artifact(artifact_name, artifact_version)
+        automl.registry.register(new_artifact)
+except AttributeError:
+    st.write("Error: please run the pipeline first.")
+except AssertionError:
+    pass
---
+++ app/pages/3_🚀_Deployment.py
@@ -0,0 +1,45 @@
+import pickle
+import streamlit as st
+from app.core.datasets_management import create_dataset
+from app.core.modelling.datasets import artifact_to_dataset, pick_dataset, select_features
+from app.core.system import AutoMLSystem
+
+
+automl = AutoMLSystem.get_instance()
+
+deployed_pipelines = automl.registry.list(type="pipeline")
+st.write("**Deployed pipelines:**")
+st.write(", ".join(x.name for x in deployed_pipelines))
+
+summary_pipeline = st.selectbox(
+    "Select a pipeline to view the summary.",
+    deployed_pipelines,
+    format_func=lambda x: x.name,
+)
+
+model = pickle.loads(summary_pipeline.data).model
+st.write(f"**Summary of {summary_pipeline.name}:**")
+st.write(f"Version: {summary_pipeline.version}")
+st.write(f"type: {summary_pipeline.type}")
+st.write(f"Model: {model.__class__.__name__}")
+
+predictions_csv = st.file_uploader("**Upload a CSV file for predictions:**", type="csv")
+create_dataset(predictions_csv)
+selected_dataset = pick_dataset()
+input_features, target_feature = select_features(selected_dataset)
+
+predictions = model.predict(predictions_csv)
+st.write(predictions)
+st.write("Download the predictions.")
+st.download_button(
+    label="Download predictions",
+    data=predictions,
+    file_name="predictions.csv",
+    mime="text/csv",
+)
+st.write("Deploy the pipeline.")
+if st.button("Deploy"):
+    automl.deploy(summary_pipeline)
+    st.write("Pipeline deployed successfully.")
+    st.write("Refresh the page to view the updated list of deployed pipelines.")
+    st.balloons()
